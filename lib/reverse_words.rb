57649e98d3c04700a2cd0bef6db38ea3643e773c
Matt McKnett <mmcknett@gmail.com>
2019-03-30 17:39:56 -0700

    Implement reverse_words

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -2,5 +2,35 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if my_words.nil?
+
+  first = 0
+  last = 0
+  while last <= my_words.length
+    if my_words[last] == " " || my_words[last].nil?
+      # End of a word
+      string_reverse(my_words, first, last)
+      last += 1
+      first = last
+    else
+      last += 1
+    end
+  end
+end
+
+def string_reverse(my_string, first, last)
+  if my_string == nil || my_string == "" || last - first <= 1 then
+    return my_string
+  end
+
+  # Until you reach the middle of the string, swap
+  # a character from the front half with a character
+  # from the back half.
+  while first < last do
+    my_string[first], my_string[last - 1] = my_string[last - 1], my_string[first]
+    first += 1
+    last -= 1
+  end
+
+  return my_string
 end
--- specs/reverse_words_spec.rb
+++ specs/reverse_words_spec.rb
@@ -2,9 +2,60 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/reverse_words'
 
+describe "String reverse" do
+  it "Must reverse a full string" do
+    # Arrange
+    input = "Hello"
+    expected = "olleH"
+
+    # Act
+    output = string_reverse(input, 0, input.length)
+
+    # Assert
+    output.must_equal expected
+  end
+
+  it "Must reverse an arbitrary substring" do
+    # Arrange
+    input = "Hello there!"
+    expected = "Holle there!"
+
+    # Act
+    output = string_reverse(input, 1, 5)
+
+    # Assert
+    output.must_equal expected
+  end
+
+  it "Must noop if one character is specified" do
+    # Arrange
+    input = "Hello there!"
+    expected = "Hello there!"
+
+    # Act
+    output = string_reverse(input, 1, 2)
+
+    # Assert
+    output.must_equal expected
+  end
+
+  it "Must noop if no character is specified" do
+    # Arrange
+    input = "Hello there!"
+    expected = "Hello there!"
+
+    # Act
+    output = string_reverse(input, 1, 1)
+
+    # Assert
+    output.must_equal expected
+  end
+end
+
 describe "reverse words" do
   describe "basic tests" do
     it "reverse each word in a sentence with one word" do
+      
       test_string = "Engineer"
 
       reverse_words(test_string)
